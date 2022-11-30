a2ddad1ee5e279b9c364e3f0e8e421543a772fb0
Reian Beltran <rmb20j@fsu.edu>
2022-11-29 23:29:01 -0500

    drums work

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 var cnv;
 let bgImg;
 let jingle = [];
+let drum
 let capture;
 
 function centerCanvas() {
@@ -12,12 +13,14 @@ function centerCanvas() {
 function preload(){
   bgImg = loadImage("assets/caroling.png");
   soundFormats('wav');
-  for (let i = 0; i < 2; i++){
+  for (let i = 0; i < 3; i++){
   jingle[i] = loadSound("assets/jingle.wav");
   }
+  drum = loadSound("assets/drum.wav");
 }
 function setup() {
   cnv = createCanvas(1100, 800);
+  cnv.mousePressed();
   centerCanvas();
 }
 
@@ -31,5 +34,12 @@ function mouseMoved() {
     jingle[0].play();
   } else if (jingle[1].isPlaying()==false){
     jingle[1].play();
+  } else if (jingle[2].isPlaying()==false){
+    jingle[2].play();
   }
+}
+
+function mousePressed() {
+  drum.play();
+  console.log("mouse is pressed");
 }
