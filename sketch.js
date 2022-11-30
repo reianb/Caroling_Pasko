a7fc7a885377802faa6849f9336a6ed8a68df5c0
Reian Beltran <rmb20j@fsu.edu>
2022-11-29 22:48:53 -0500

    added image

--- sketch.js
+++ sketch.js
@@ -1,15 +1,24 @@
 var cnv;
+let bgImg;
+let jingle;
 
 function centerCanvas() {
   var x = (windowWidth - width) / 2;
   var y = (windowHeight - height) / 2;
   cnv.position(x, y);
 }
+
+function preload(){
+  bgImg = loadImage("assets/caroling.png");
+  soundFormats('wav');
+  jingle = loadSound("assets/jingle.wav");
+}
 function setup() {
-  cnv = createCanvas(1000, 800);
+  cnv = createCanvas(1100, 800);
   centerCanvas();
 }
 
 function draw() {
   background(125);
+  image(bgImg,0,0);
 }
